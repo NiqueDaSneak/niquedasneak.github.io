9483a46d587480d6b71ea919f887c5b1b185aeb2
Dom Clemmer <thisisdom4@gmail.com>
2019-02-26 19:52:35 -0500

    initial commit

---
+++ src/components/AnimatedLogo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import shipLogo from '../assets/imgs/ship-logo.png'
+
+const AnimatedLogo = () => (
+  <img src={shipLogo} />
+)
+
+export default AnimatedLogo
---
+++ src/components/LandingScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import AnimatedLogo from './AnimatedLogo'
+
+
+const LandingScreen = () => (
+  <AnimatedLogo />
+)
+
+export default LandingScreen
