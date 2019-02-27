71b149c4825fe6eaed38fd3e36d5a4792a65bbbd
Dom Clemmer <thisisdom4@gmail.com>
2019-02-26 21:07:23 -0500

    animated logo finished

--- src/components/AnimatedLogo.js
+++ src/components/AnimatedLogo.js
@@ -1,8 +1,17 @@
 import React from 'react'
+
 import shipLogo from '../assets/imgs/ship-logo.png'
+import shipLogoGold from '../assets/imgs/ship-logo-gold.png'
+
+import '../assets/sass/logo.sass'
 
 const AnimatedLogo = () => (
-  <img src={shipLogo} />
+  <div className='logoContainer'>
+    <img className='ship1'src={shipLogoGold} />
+    <img className='ship2'src={shipLogo} />
+    <img className='ship3'src={shipLogo} />
+    <img className='ship4'src={shipLogo} />
+  </div>
 )
 
 export default AnimatedLogo
