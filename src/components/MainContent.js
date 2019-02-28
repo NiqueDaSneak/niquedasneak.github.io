eb3a06a6b43005b3949ba9b21f36bfbbc1139764
Dom Clemmer <thisisdom4@gmail.com>
2019-02-28 13:09:15 -0700

    restructure

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import '../assets/sass/maincontent.sass'
+import Slider from './Slider'
+
+const MainContent = () => (
+    <div className='container'>
+      <span className='title'>I'm a Front-End Developer Specializing in...</span>
+      <Slider />
+    </div>
+)
+
+export default MainContent
