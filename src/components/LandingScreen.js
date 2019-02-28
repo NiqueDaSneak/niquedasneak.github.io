b43500adea4676d0c40555c74255e858d5f94276
Dom Clemmer <thisisdom4@gmail.com>
2019-02-27 19:44:05 -0500

    animate down chevron

--- src/components/LandingScreen.js
+++ src/components/LandingScreen.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React,{ Component }  from 'react'
 
 import '../assets/sass/landingScreen.sass'
 import AnimatedLogo from './AnimatedLogo'
 import ExternalLinks from './ExternalLinks'
+import downChevronImg from '../assets/imgs/down-chevron.svg'
 
-const LandingScreen = () => (
-  <div className='landingScreen'>
-  <AnimatedLogo />
-  <header>
-    <span className='headerName'>Dominique Clemmer</span>
-    <span className='headerTitle'>Front End Developer</span>
-  </header>
-  <ExternalLinks />
-  </div>
-)
+class LandingScreen extends Component {
+  state = {
+    statements: [
+      'String 1',
+      'This is String 2',
+      'Hello from String 3'
+    ],
+    dynamicInput: ''
+  }
+
+  componentDidMount() {
+    // this is where my logic goes for the type out functionlaity
+  }
+
+  render() {
+    return(
+      <div className='landingScreen'>
+        <AnimatedLogo />
+        <header>
+          <span className='headerName'>Dominique Clemmer</span>
+          <span className='headerTitle'>{ this.state.dynamicInput }</span>
+        </header>
+        <ExternalLinks />
+        <img className='downChevronImg' src={downChevronImg } />
+      </div>
+    )
+  }
+}
 
 export default LandingScreen
