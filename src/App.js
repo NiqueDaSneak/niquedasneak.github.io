6a8eb42d910fb5d2aa13c44b141a652e0d6dc5d0
Dom Clemmer <thisisdom4@gmail.com>
2019-02-28 12:49:44 -0700

    rename components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './assets/sass/App.sass';
 
 import LandingScreen from './components/LandingScreen'
+import MainContent from './components/MainContent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <LandingScreen />
+        <MainContent />
       </div>
     );
   }
