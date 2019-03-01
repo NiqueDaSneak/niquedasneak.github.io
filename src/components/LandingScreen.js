164a270fb24db64d3828f8a4550258c6fc9296e9
Dom Clemmer <thisisdom4@gmail.com>
2019-02-28 18:08:57 -0700

    add Typist for type out effect

--- src/components/LandingScreen.js
+++ src/components/LandingScreen.js
@@ -1,37 +1,40 @@
 import React,{ Component }  from 'react'
+import Typist from 'react-typist'
 
 import '../assets/sass/landingScreen.sass'
 import AnimatedLogo from './AnimatedLogo'
 import ExternalLinks from './ExternalLinks'
 import downChevronImg from '../assets/imgs/down-chevron.svg'
 
-class LandingScreen extends Component {
-  state = {
-    statements: [
-      'String 1',
-      'This is String 2',
-      'Hello from String 3'
-    ],
-    dynamicInput: ''
-  }
+const LandingScreen = () => (
+  <div className='landingScreen'>
+    <AnimatedLogo />
+    <header>
+      <span className='headerName'>Dominique Clemmer</span>
+      <Typist>
+        <span className='headerTitle'>Front-End Developer</span>
+        <Typist.Backspace count={19} delay={750}/>
+        <span className='headerTitle'>Futurist</span>
+        <Typist.Backspace count={8} delay={750}/>
+        <span className='headerTitle'>Yogi</span>
+        <Typist.Backspace count={4} delay={750}/>
+        <span className='headerTitle'>Empath</span>
+        <Typist.Backspace count={6} delay={750}/>
+        <span className='headerTitle'>Gym Rat</span>
+        <Typist.Backspace count={7} delay={750}/>
+        <span className='headerTitle'>Basketball Fiend</span>
+        <Typist.Backspace count={16} delay={750}/>
+        <span className='headerTitle'>Kobe Fan</span>
+        <Typist.Backspace count={8} delay={750}/>
+        <span className='headerTitle'>Traveler</span>
+        <Typist.Backspace count={8} delay={750}/>
+        <span className='headerTitle'>Human Being</span>
+    </Typist>
+    </header>
+    <ExternalLinks />
+    <img className='downChevronImg' src={downChevronImg } />
+  </div>
+)
 
-  componentDidMount() {
-    // this is where my logic goes for the type out functionlaity
-  }
-
-  render() {
-    return(
-      <div className='landingScreen'>
-        <AnimatedLogo />
-        <header>
-          <span className='headerName'>Dominique Clemmer</span>
-          <span className='headerTitle'>{ this.state.dynamicInput }</span>
-        </header>
-        <ExternalLinks />
-        <img className='downChevronImg' src={downChevronImg } />
-      </div>
-    )
-  }
-}
 
 export default LandingScreen
