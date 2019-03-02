212153e82c9404e38cc391cc66a4d60e16d969b8
Dom Clemmer <thisisdom4@gmail.com>
2019-03-02 11:50:35 -0700

    build and style popout component

--- src/components/Popout.js
+++ src/components/Popout.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Btn from './Btn'
 
-const popout = ( props ) => (
-  <div>
-    <span>{ props.copy }</span>
-  </div>
-)
+import githubIcon from '../assets/imgs/github.svg'
+import '../assets/sass/popout.sass'
+
+const popout = ( props ) => {
+  var btns
+
+  switch (props.actionable) {
+    case 'SHOWEXAMPLES':
+      btns = [<Btn title='See What I Mean' link='google.com' img={githubIcon}/>]
+      break
+    case 'LINKTOGITHUB':
+      btns = [<Btn title='GitHub' link='google.com' img={githubIcon}/>]
+      break
+    case'LINKTOEXAMPLES':
+      btns = [<Btn title='U of Cincy' link='google.com' img={githubIcon}/>,<Btn title='5/3' link='google.com' img={githubIcon}/>,<Btn title='Bridgeable' link='google.com' img={githubIcon}/>]
+      break
+  }
+  return (
+    <div className='popoutContainer'>
+      <span className='popoutHeading'>{ props.copy }</span>
+      <div className='btnsContainer'>{ btns }</div>
+    </div>
+  )
+}
 
 export default popout
