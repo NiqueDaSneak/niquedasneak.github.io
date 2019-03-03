99bfd7d7728feae71e50cd3aca0b963ce57026b2
Dom Clemmer <thisisdom4@gmail.com>
2019-03-02 17:26:59 -0700

    add new styles for tldr

--- src/components/TLDR.js
+++ src/components/TLDR.js
@@ -3,6 +3,11 @@ import React from 'react'
 import Btn from './Btn'
 import githubIcon from '../assets/imgs/github.svg'
 import cvIcon from '../assets/imgs/cv.svg'
+import ucIcon from '../assets/imgs/uc.svg'
+import fiveThreeIcon from '../assets/imgs/53.svg'
+import rightArrowIcon from '../assets/imgs/right-arrow.svg'
+
+
 import '../assets/sass/tldr.sass'
 
 const TLDR = ( props ) => (
@@ -13,27 +18,25 @@ const TLDR = ( props ) => (
         <span>TL;DR</span>
       </div>
       <section>
-        <span className='sectionHeader'>Section Header</span>
+        <span className='sectionHeader'>Quick Links</span>
         <div className='slidebtnContainer first'>
-          <Btn title='GitHub' img={githubIcon} link='GitHub' />
-          <Btn title='GitHub' img={cvIcon} link='Resume' />
+          <Btn bgColor='#192F00' title='GitHub' img={githubIcon} link='GitHub' />
+          <Btn bgColor='#192F00' title='Resume' img={cvIcon} link='Resume' />
         </div>
       </section>
       <section>
-        <span className='sectionHeader'>Section Header</span>
-        <ul style={ {margin: 0} }>
-          <li>Lorem Ipsume dolor emit tisla</li>
-          <li>Lorem Ipsume dolor emit tisla</li>
-          <li>Lorem Ipsume dolor emit tisla</li>
-          <li>Lorem Ipsume dolor emit tisla</li>
+        <span className='sectionHeader'>Quick Facts</span>
+        <ul style={ {margin: 0, color: 'white', textAlign: 'left', width: '50vw'} }>
+          <li>I design & I code</li>
+          <li>JavaScript/React/React Native is my jam</li>
         </ul>
       </section>
       <section>
-        <span className='sectionHeader'>Section Header</span>
+        <span className='sectionHeader'>Quick Portfolio</span>
         <div className='slidebtnContainer last'>
-          <Btn title='GitHub' img={githubIcon} link='' />
-          <Btn title='GitHub' img={cvIcon} link='' />
-          <Btn title='GitHub' img={githubIcon} link='' />
+          <Btn bgColor='white' title='UC' img={ucIcon} link='' />
+          <Btn bgColor='#1E3D97' title='5/3' img={fiveThreeIcon} link='' />
+          <Btn bgColor='#57008E' title='Bridge' img={rightArrowIcon} link='' />
         </div>
       </section>
     </section>
