908ffb316822b8935c60927a977479cffd1f7ea8
Dom Clemmer <thisisdom4@gmail.com>
2019-03-04 01:21:51 -0700

    add new imgs

--- src/components/Popout.js
+++ src/components/Popout.js
@@ -6,8 +6,8 @@ import fiveThreeIcon from '../assets/imgs/53.svg'
 import rightArrowIcon from '../assets/imgs/right-arrow.svg'
 import githubIcon from '../assets/imgs/github.svg'
 import reactImg from '../assets/imgs/react.svg'
-import codeImg from '../assets/imgs/code.svg'
-import cubeImg from '../assets/imgs/cube.svg'
+import whiteCodeImg from '../assets/imgs/white-code.svg'
+import whiteCubeImg from '../assets/imgs/white-cube.svg'
 
 import '../assets/sass/popout.sass'
 
@@ -18,20 +18,21 @@ const popout = ( props ) => {
   switch (props.actionable) {
     case 'SHOWEXAMPLES':
       btns = [<Btn title='See What I Mean' link='google.com' img={rightArrowIcon}/>]
-      correspondingImg = <img src={reactImg}/>
+      correspondingImg = <img style={{width: '36vw'}} src={whiteCubeImg}/>
       break
     case 'LINKTOGITHUB':
       btns = [<Btn title='GitHub' link='https://www.github.com/niquedasneak' img={githubIcon}/>]
-      correspondingImg = <img src={codeImg}/>
+      correspondingImg = <img style={{width: '36vw'}} src={reactImg}/>
       break
     case'LINKTOEXAMPLES':
       btns = [<Btn bgColor='white' title='UC' img={ucIcon} link='' />, <Btn bgColor='#1E3D97' title='5/3' img={fiveThreeIcon} link='' />, <Btn bgColor='#57008E' title='Bridge' img={rightArrowIcon} link='' />]
-      correspondingImg = <img src={cubeImg}/>
+      correspondingImg = <img style={{width: '36vw'}} src={whiteCodeImg}/>
       break
   }
   return (
     <div className='popoutContainer'>
       {correspondingImg}
+      <hr style={{ border: '1px solid white', width: '100%' }}/>
       <span className='popoutHeading'>{ props.copy }</span>
       <div className='popoutbtnsContainer'>{ btns }</div>
     </div>
